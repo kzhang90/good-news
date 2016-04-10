80bfe1a9118fbac236b6c1fad6f5acb8bfceaa60
Kenny Zhang <Kenny.y.zhang@gmail.com>
2016-04-09 17:20:10 -0700

    results returning after refactor,still some bugs

--- client/js/controllers.js
+++ client/js/controllers.js
@@ -1,12 +1,21 @@
 app.controller('MainCtrl', ['$scope','$http','keyGetter','articleFinder', function($scope, $http, keyGetter, articleFinder) {
   $scope.obj = {};
-  var topic = $scope.obj.topic;
   var url = "https://gateway-a.watsonplatform.net/calls/data/GetNews?outputMode=json&start=now-1d&end=now&count=5&q.enriched.url.enrichedTitle.docSentiment=|type=positive,score=>0.9|&return=enriched.url.url,enriched.url.title";
-  var key = keyGetter.findKey;
   
+  keyGetter.findKey().then(function(data) {
+    $scope.key = data.data.key;
+  });
+
+  var news = articleFinder.getNews;
+
   $scope.someFunction = function() {
-    news.then(function(response) {
-      $scope.articles = response;
+    var topic = $scope.obj.topic;
+    var key = $scope.key;
+    console.log(topic);
+    console.log(url);
+    console.log(key);
+    news(url,topic,key).then(function(data) {
+      $scope.articles = data.data.result.docs;
     });
   }
 
