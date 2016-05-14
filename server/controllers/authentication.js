96e4910d5c670860a8594d1ff36fcf29c600d32b
Kenny Zhang <Kenny.y.zhang@gmail.com>
2016-05-14 13:51:22 -0700

    finish serverside auth

---
+++ server/controllers/authentication.js
@@ -0,0 +1,43 @@
+var passport = require('passport');
+var mongoose = require('mongoose');
+var User = mongoose.model('User');
+
+// code needs error traps
+// validate form inputs
+// catch errors in the save function
+module.exports.register = function(req, res) {
+  var user = new User();
+
+  user.name = req.body.name;
+  user.email = req.body.email;
+
+  user.setPassword(req.body.password);
+
+  user.save(function(err) {
+    var token = user.generateJwt();
+    res.status(200);
+    res.json({
+      'token': token
+    });
+  });
+}
+
+module.exports.login = function(req, res) {
+
+  passport.authenticate('local', function(err, user, info) {
+    if (err) {
+      res.status(404).json(err);
+      return;
+    }
+    if (user) {
+      var token = user.generateJwt();
+      res.status(200);
+      res.json({
+        'token': token
+      });
+    } else {
+      // what does info mean right here?
+      res.status(401).json(info);
+    }
+  });
+}
